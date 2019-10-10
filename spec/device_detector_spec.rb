32769e9814c4e7e8cfdc7b33031448ed1a5f23f9
Anton Rieder <aried3r@gmail.com>
2019-10-10 16:39:49 +0200

    Remove minitest deprecations
    The global `.must_equal` and `.must_be_nil` will be removed in
    minitest 6. There's three options:
    `_(obj).must_equal`
    `value(obj).must_equal`
    `expect(obj).must_equal`.
    
    I chose value(obj) because it reads the most correct to me.
    See also: https://stackoverflow.com/a/58128950

--- spec/device_detector_spec.rb
+++ spec/device_detector_spec.rb
@@ -13,49 +13,49 @@ describe DeviceDetector do
 
       describe '#name' do
         it 'returns the name' do
-          client.name.must_equal 'Chrome'
+          value(client.name).must_equal 'Chrome'
         end
       end
 
       describe '#full_version' do
         it 'returns the full version' do
-          client.full_version.must_equal '30.0.1599.69'
+          value(client.full_version).must_equal '30.0.1599.69'
         end
       end
 
       describe '#os_family' do
         it 'returns the operating system name' do
-          client.os_family.must_equal 'Mac'
+          value(client.os_family).must_equal 'Mac'
         end
       end
 
       describe '#os_name' do
         it 'returns the operating system name' do
-          client.os_name.must_equal 'Mac'
+          value(client.os_name).must_equal 'Mac'
         end
       end
 
       describe '#os_full_version' do
         it 'returns the operating system full version' do
-          client.os_full_version.must_equal '10.8.5'
+          value(client.os_full_version).must_equal '10.8.5'
         end
       end
 
       describe '#known?' do
         it 'returns true' do
-          client.known?.must_equal true
+          value(client.known?).must_equal true
         end
       end
 
       describe '#bot?' do
         it 'returns false' do
-          client.bot?.must_equal false
+          value(client.bot?).must_equal false
         end
       end
 
       describe '#bot_name' do
         it 'returns nil' do
-          client.bot_name.must_be_nil
+          value(client.bot_name).must_be_nil
         end
       end
     end
@@ -67,13 +67,13 @@ describe DeviceDetector do
 
       describe '#os_family' do
         it 'returns the operating system name' do
-          client.os_family.must_equal 'GNU/Linux'
+          value(client.os_family).must_equal 'GNU/Linux'
         end
       end
 
       describe '#os_name' do
         it 'returns the operating system name' do
-          client.os_name.must_equal 'Ubuntu'
+          value(client.os_name).must_equal 'Ubuntu'
         end
       end
     end
@@ -82,7 +82,7 @@ describe DeviceDetector do
       let(:user_agent) { 'Mozilla/5.0 (Android 7.0; Mobile; rv:53.0) Gecko/53.0 Firefox/53.0' }
 
       it 'detects smartphone' do
-        client.device_type.must_equal 'smartphone'
+        value(client.device_type).must_equal 'smartphone'
       end
     end
 
@@ -90,7 +90,7 @@ describe DeviceDetector do
       let(:user_agent) { 'Mozilla/5.0 (Android 6.0.1; Tablet; rv:47.0) Gecko/47.0 Firefox/47.0' }
 
       it 'detects tablet' do
-        client.device_type.must_equal 'tablet'
+        value(client.device_type).must_equal 'tablet'
       end
     end
   end
@@ -100,43 +100,43 @@ describe DeviceDetector do
 
     describe '#name' do
       it 'returns nil' do
-        client.name.must_be_nil
+        value(client.name).must_be_nil
       end
     end
 
     describe '#full_version' do
       it 'returns nil' do
-        client.full_version.must_be_nil
+        value(client.full_version).must_be_nil
       end
     end
 
     describe '#os_name' do
       it 'returns nil' do
-        client.os_name.must_be_nil
+        value(client.os_name).must_be_nil
       end
     end
 
     describe '#os_full_version' do
       it 'returns nil' do
-        client.os_full_version.must_be_nil
+        value(client.os_full_version).must_be_nil
       end
     end
 
     describe '#known?' do
       it 'returns false' do
-        client.known?.must_equal false
+        value(client.known?).must_equal false
       end
     end
 
     describe '#bot?' do
       it 'returns false' do
-        client.bot?.must_equal false
+        value(client.bot?).must_equal false
       end
     end
 
     describe '#bot_name' do
       it 'returns nil' do
-        client.bot_name.must_be_nil
+        value(client.bot_name).must_be_nil
       end
     end
   end
@@ -146,43 +146,43 @@ describe DeviceDetector do
 
     describe '#name' do
       it 'returns nil' do
-        client.name.must_be_nil
+        value(client.name).must_be_nil
       end
     end
 
     describe '#full_version' do
       it 'returns nil' do
-        client.full_version.must_be_nil
+        value(client.full_version).must_be_nil
       end
     end
 
     describe '#os_name' do
       it 'returns nil' do
-        client.os_name.must_be_nil
+        value(client.os_name).must_be_nil
       end
     end
 
     describe '#os_full_version' do
       it 'returns nil' do
-        client.os_full_version.must_be_nil
+        value(client.os_full_version).must_be_nil
       end
     end
 
     describe '#known?' do
       it 'returns false' do
-        client.known?.must_equal false
+        value(client.known?).must_equal false
       end
     end
 
     describe '#bot?' do
       it 'returns true' do
-        client.bot?.must_equal true
+        value(client.bot?).must_equal true
       end
     end
 
     describe '#bot_name' do
       it 'returns the name of the bot' do
-        client.bot_name.must_equal 'Googlebot'
+        value(client.bot_name).must_equal 'Googlebot'
       end
     end
   end
