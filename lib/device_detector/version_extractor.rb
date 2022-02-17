6d30b7b9d5d84125d54edf33c79d81767a004b8e
Robert Wachs <robert@podigee.com>
2022-02-17 15:16:35 +0100

    os version extraction uses additional regex list
    for more detailled version mapping

--- lib/device_detector/version_extractor.rb
+++ lib/device_detector/version_extractor.rb
@@ -8,10 +8,29 @@ class DeviceDetector
     MAJOR_VERSION_8 = Gem::Version.new('8.0')
 
     def call
-      super&.chomp('.')
+      simple_version = super&.chomp('.')
+
+      return simple_version unless simple_version&.empty?
+
+      os_version_by_regexes
     end
 
     private
+
+    def os_version_by_regexes
+      version_matches = regex_meta[:versions]
+      return '' unless version_matches
+
+      version_matches.detect do |matcher|
+        user_agent.match(matcher[:regex]) do |match_data|
+          return matcher[:version].gsub(/\$(\d)/) do
+            match_data[Regexp.last_match(1).to_i].to_s
+          end.strip
+        end
+      end
+
+      ''
+    end
 
     def metadata_string
       String(regex_meta[:version])
